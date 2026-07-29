88c80cc7be248c9ca9f18b675444df19aef3da92
DiegoDFB <f.blanco@aluno.ifsp.edu.br>
2026-07-29 23:02:25 +0000

    Adicionado rotas e template

--- hello.py
+++ hello.py
@@ -1,10 +1,38 @@
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask
+from flask import Flask, render_template, request, abort, make_response, redirect
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return '<p>Alterações por meio do PythonAnyWhere -> GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+    return '<h1>Hello World!</h1><h2>Disciplina PTBDSWS</h2>'
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+@app.route('/contextorequisicao')
+def contextorequisicao():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('400.html'), 400
+
+@app.route('/codigostatusdiferente')
+def codigostatusdiferente():
+    abort(400);
+
+@app.route('/objetoresposta')
+def objetoresposta():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('cookie_teste', '12345')
+    return response
+
+@app.route('/redirecionamento')
+def redirecionamento():
+    return redirect('https://ptb.ifsp.edu.br')
+
+@app.route('/abortar')
+def abortar():
+    abort(404);
