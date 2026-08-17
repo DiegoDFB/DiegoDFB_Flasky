258d6a56945e1f195750a5a714f5b55f4ab38a9f
DiegoDFB <f.blanco@aluno.ifsp.edu.br>
2026-08-17 16:33:17 +0000

    feat: adicionado forms

--- hello.py
+++ hello.py
@@ -1,16 +1,31 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask, render_template, request, abort, make_response, redirect
+from flask import Flask, render_template, request, abort, make_response, redirect, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'DiegoDFBPT3036278'
+
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('home'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>/<prontuario>/<instituicao>')
