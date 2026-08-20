00b2b03deae6b75be6c9d0025941c61e994437e4
DiegoDFB <f.blanco@aluno.ifsp.edu.br>
2026-08-20 01:50:18 +0000

    feat: adicionados forms Home e Login

--- hello.py
+++ hello.py
@@ -1,68 +1,75 @@
-from flask import Flask, render_template, request, abort, make_response, redirect, session, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'DIEGODFBPT3036278'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class HomeForm(FlaskForm):
+    nome = StringField('Informe o seu nome', validators=[DataRequired()])
+    sobrenome = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
+    instituicao = StringField('Informe a sua Insituição de ensino:', validators=[DataRequired()])
+    disciplina = SelectField('Informe a sua disciplina:', choices=[
+        ('DSWA5', 'DSWA5'),
+        ('DWBA4', 'DWBA4'),
+        ('Gestão de Projetos', 'Gestão de Projetos')
+    ], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'DiegoDFBPT3036278'
-
-moment = Moment(app)
-bootstrap = Bootstrap(app)
+class LoginForm(FlaskForm):
+    usuario = StringField('Usuário ou e-mail', validators=[DataRequired()])
+    senha = PasswordField('Informe a sua senha', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
-        return redirect(url_for('home'))
-    return render_template('index.html', form=form, name=session.get('name'))
-
-
-@app.route('/user/<name>/<prontuario>/<instituicao>')
-def user(name, prontuario, instituicao):
-    return render_template('user.html', name=name, prontuario=prontuario, instituicao=instituicao)
-
-@app.route('/contextorequisicao/<name>')
-def contextorequisicao(name):
-    user_agent = request.headers.get('User-Agent')
+    form = HomeForm()
 
     if request.headers.get('X-Forwarded-For'):
         ip = request.headers.get('X-Forwarded-For').split(',')[0].strip()
     else:
         ip = request.remote_addr
-
     host = request.host
 
-    return render_template('requisicao.html', user_agent=user_agent, ip=ip, host=host, name=name)
+    if form.validate_on_submit():
+        session['nome'] = form.nome.data
+        session['sobrenome'] = form.sobrenome.data
+        session['instituicao'] = form.instituicao.data
+        session['disciplina'] = form.disciplina.data
+        return redirect(url_for('home'))
 
-@app.errorhandler(400)
-def bad_request(e):
-    return render_template('400.html'), 400
+    return render_template(
+        'index.html',
+        form=form,
+        nome=session.get('nome'),
+        sobrenome=session.get('sobrenome'),
+        instituicao=session.get('instituicao'),
+        disciplina=session.get('disciplina'),
+        ip=ip,
+        host=host,
+        current_time=datetime.utcnow()
+    )
 
-@app.route('/codigostatusdiferente')
-def codigostatusdiferente():
-    abort(400);
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['usuario'] = form.usuario.data
+        return redirect(url_for('login_response'))
 
-@app.route('/objetoresposta')
-def objetoresposta():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('cookie_teste', '12345')
-    return response
+    return render_template('login.html', form=form, current_time=datetime.utcnow())
 
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://ptb.ifsp.edu.br')
+@app.route('/loginResponse')
+def login_response():
+    usuario = session.get('usuario')
+    return render_template('loginResponse.html', usuario=usuario, current_time=datetime.utcnow())
 
-@app.route('/abortar')
-def abortar():
-    abort(404);
+if __name__ == '__main__':
+    app.run(debug=True)
