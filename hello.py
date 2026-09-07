19b54707aaf82597f1e40e22798935b6d20cce42
DiegoDFB <f.blanco@aluno.ifsp.edu.br>
2026-09-07 18:53:37 +0000

    feat: adicionada lista de usuários cadastrados

--- hello.py
+++ hello.py
@@ -68,7 +68,8 @@ def index():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            user_role = Role.query.filter_by(name='User').first()
+            user = User(username=form.name.data, role=user_role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -76,5 +77,10 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
+
+    users = User.query.all()
+    return render_template('index.html',
+                           form=form,
+                           name=session.get('name'),
+                           known=session.get('known', False),
+                           users=users)
